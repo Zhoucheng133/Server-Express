43c723890fb2a9b4e1c7ab9159e18f3a9a4b6a2b
zhoucheng <zhoucheng133@outlook.com>
2025-11-26 17:33:41 +0800

    添加初始化语言

---
+++ lib/getx/general_controller.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class GeneralController extends GetxController {
+  Rx<Locale> lang=Rx(Locale("en", "US"));
+
+  late SharedPreferences prefs;
+
+  Future<void> init() async {
+    prefs=await SharedPreferences.getInstance();
+
+    String? languageCode=prefs.getString("languageCode");
+    String? countryCode=prefs.getString("countryCode");
+
+    if(languageCode==null || countryCode==null){
+      final sysLang=Platform.localeName;
+      languageCode = sysLang.split('_')[0];
+      countryCode = sysLang.split('_').last;
+      lang.value=Locale(languageCode, countryCode);
+      Get.updateLocale(lang.value);
+    }else{
+      lang.value=Locale(languageCode, countryCode);
+      Get.updateLocale(lang.value);
+    }
+  }
+
+  GeneralController(){
+    init();
+  }
+}
