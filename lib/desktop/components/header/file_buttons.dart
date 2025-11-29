52e78a16b8ac0641023945a099e4179a9bcb0182
zhoucheng <zhoucheng133@outlook.com>
2025-11-29 20:49:10 +0800

    添加回去上层目录

--- lib/desktop/components/header/file_buttons.dart
+++ lib/desktop/components/header/file_buttons.dart
@@ -5,6 +5,7 @@ import 'package:server_express/desktop/components/header/header_button_item.dart
 import 'package:server_express/getx/file_controller.dart';
 import 'package:server_express/getx/server_controller.dart';
 import 'package:server_express/getx/ssh_controller.dart';
+import 'package:path/path.dart' as p;
 
 class FileButtons extends StatefulWidget {
   const FileButtons({super.key});
@@ -31,13 +32,22 @@ class _FileButtonsState extends State<FileButtons> {
     await fileController.getFiles(context);
   }
 
+  void parentFolder(BuildContext context) async {
+    if(fileController.path.value=="/"){
+      return;
+    }
+    fileController.path.value=p.dirname(fileController.path.value);
+    await fileController.getFiles(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         HeaderButtonItem(buttonSide: ButtonSide.left, func: ()=>disconnectServer(context), icon: Icons.link_off_rounded, text: "disconnect".tr),
-        HeaderButtonItem(buttonSide: ButtonSide.mid, func: ()=>refreshFiles(context), icon: Icons.refresh_rounded, text: "refresh".tr),
+        HeaderButtonItem(buttonSide: ButtonSide.mid, func: ()=>parentFolder(context), icon: Icons.keyboard_arrow_up_rounded, text: "parentFolder".tr),
         HeaderButtonItem(buttonSide: ButtonSide.mid, func: ()=>{}, icon: Icons.check_box_rounded, text: "select".tr),
+        HeaderButtonItem(buttonSide: ButtonSide.right, func: ()=>refreshFiles(context), icon: Icons.refresh_rounded, text: "refresh".tr),
       ],
     );
   }
