52d7a564538cac2c172d7a79c67b3e80f4f89fb8
zhoucheng <zhoucheng133@outlook.com>
2025-12-29 00:02:18 +0800

    删除一个todo

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,6 @@ class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     final brightness = MediaQuery.of(context).platformBrightness;
-    // TODO 记住深色模式？
     controller.darkModeHandler(brightness);
 
     return Obx(()=>
